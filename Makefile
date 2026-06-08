e85c24dd165cc41fdb25b7eb6e812c6559672fa9
Jim <frede061665@gmail.com>
2026-06-08 13:08:09 +0300

    fix Makefile, internal/http-server/handlers/interfaces.go, add unit test internal/http-server/handlers/tasks_test.go #3

--- Makefile
+++ Makefile
@@ -1,11 +1,13 @@
-.PHONY: state, up, down, seed, test
+.PHONY: state, up, down, seed, test, all
+
+all: up start 
 
 start:
 	CONFIG_PATH=./config/local.yaml go run main.go
 
 up:
-	docker compose up --build
-	// TODO: Доавить контейнер с веб сервисом или в docker compose web сервер
+	docker compose up --build -d
+	# TODO: Доавить контейнер с веб сервисом или в docker compose web сервер
 
 down:
 	docker compose down
@@ -14,7 +16,7 @@ seed:
 	@echo "Seeding database..."
 	docker compose exec -T db psql -U postgres -d app < ./seed.sql
 	@echo "Database seeded successfully."
-	// TODO: Понадобиться для тестов воркеров
+	# TODO: Понадобиться для тестов воркеров
 
 test:
 	go test ./... -v
---
+++ internal/http-server/handlers/tasks_test.go
@@ -0,0 +1,84 @@
+package handlers_test
+
+import (
+	"OctoQueue/internal/http-server/handlers"
+	"OctoQueue/internal/storage"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+
+	"OctoQueue/internal/storage/mocks"
+	"log/slog"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+func TestCreateTask(t *testing.T) {
+	tests := []struct {
+		log     *slog.Logger
+		name    string
+		st      mocks.TaskRepository
+		want    http.HandlerFunc
+		wantErr bool
+	}{
+		{
+			name: "base test",
+			st:   mocks.TaskRepository{},
+		},
+	} // TODO: add more test cases
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			repo := mocks.NewTaskRepository(t)
+
+			repo.
+				On(
+					"CreateTask",
+					tt.log,
+					mock.Anything,
+				).
+				Return(
+					&storage.Task{
+						ID: mock.Anything,
+					},
+					nil,
+				)
+
+			handler := handlers.CreateTask(
+				slog.Default(),
+				repo,
+			)
+
+			body := `{
+				"Name":       "Daily backup",
+				"Type":       "http_call",
+				"Payload":    {"url":"https://example.com/api","method":"POST"},
+				"Schedule":   "0 0 * * *",
+				"Timezone":   "UTC",
+				"MaxRetries": 3,
+				"Tags":       "test",
+				"CreatedBy":  "test_user",
+				"TargetHost": "example.com",
+			}`
+
+			req := httptest.NewRequest(
+				http.MethodPost,
+				"/tasks",
+				strings.NewReader(body),
+			)
+
+			rr := httptest.NewRecorder()
+
+			handler.ServeHTTP(rr, req) 
+
+			// Check the status code
+
+			assert.Equal(
+				t,
+				http.StatusCreated,
+				rr.Code,
+			)
+		})
+	}
+}
